e4f9651f4dc231a79a2d8643ee84077220bab2be
Lera <ohhsith@ukr.net>
2021-07-21 18:53:20 +0300

    js file fixes

--- js-script.js
+++ js-script.js
@@ -12,24 +12,19 @@ showNum(2,5);
 console.log('------------- #4')
 
 let time = document.querySelector('.time');
+let showFullTime = true;
 
-function showTime(t=1) {
-    
-    let int = setInterval(function(){
-        let now = new Date();
-        let hours = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
-        let minutes = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
-        let seconds = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds(); 
-        t==1 ? time.innerHTML=`${hours}:${minutes}:${seconds}`: time.innerHTML=`${hours}:${minutes}`;
-    }, 1000);  
-     
-    time.addEventListener('click', () =>{
-        clearInterval(int);   
-        showTime(2);
-    });
-    
-} 
-showTime();
+let int = setInterval(function(){
+    let now = new Date();
+    let hours = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
+    let minutes = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
+    let seconds = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds(); 
+    showFullTime ? time.innerHTML=`${hours}:${minutes}:${seconds}`: time.innerHTML=`${hours}:${minutes}`;
+}, 1000);
+
+time.addEventListener('click', () =>{
+    showFullTime = !showFullTime;
+});
 
 
 
